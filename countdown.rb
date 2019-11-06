de49e39fc21263350cca8626dfd05efe30dfaaa6
jerrard suell <seulljay@gmail.com>
2019-11-06 14:36:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,8 +1,15 @@
-
 def countdown
   number = 10
   while number > 0 
   puts "#{number} SECOND(S)!"
+end
+  puts "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep
+  number = 10
+  while > 0
+  sleep(1)
 end
 end
 
